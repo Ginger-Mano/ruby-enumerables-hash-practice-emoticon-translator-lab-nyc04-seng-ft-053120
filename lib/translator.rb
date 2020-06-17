6bc5be62e23cfa8b8ed744cf862a9bfb3584b8f1
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-06-17 17:35:37 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,32 @@
-# require modules here
+require 'pry'
+require 'yaml' # require modules here
 
-def load_library
-  # code goes here
+def load_library(file_name)
+emoticons = YAML.load_file(file_name)
+new_hash = {}
+#binding.pry # require modules here
+emoticons.each do |key, value|
+  new_hash[key] = {
+    :english => value[0],
+    :japanese => value[1]
+  }
+end
+  new_hash
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file_name, emoticon)
+  emoticons = YAML.load_file(file_name)
+  emoticons.each do |key, value|
+    emoticons.value?(value[1])
+    value[1] = key
+    binding.pry
+  end
+    value[1]
+    #go over each key and assign the japanese value to the english name.
+    #japanese value == english name
 end
 
 def get_english_meaning
-  # code goes here
+  emoticons[:japanese] = emoticons[:english]
+  emoticons[:english]# code goes here
 end
